aa4a076b203b6d82fa0bc3f4e2bb5ce03e93cba5
MxGRRR <89796414+LeeMcGirr@users.noreply.github.com>
2023-10-02 17:22:55 -0400

    updated physics controller

--- BloomfieldFall23/Assets/physicsGame/playerController3D.cs
+++ BloomfieldFall23/Assets/physicsGame/playerController3D.cs
@@ -6,22 +6,31 @@ using UnityEngine.UIElements;
 
 public class playerController3D : MonoBehaviour
 {
+    [Header("move vars")]
     public float speed = 1f;
+    public float jumpForce = 5f;
+
+    [Header("look vars")]
     public Vector2 sensMouse = new Vector2 (.5f,.5f);
     public GameObject myCam;
     public Vector2 camVertLock = new Vector2(90,-90);
-    Vector3 myDir;
 
+    [Header("debugs")]
+    public bool grounded;
+    Rigidbody myRB;
+    Vector3 myDir;
     float rotY;
     float rotX;
     // Start is called before the first frame update
     void Start()
     {
+        grounded = false;
         myDir = Vector3.zero;
         rotY = 0f;
         rotX = 0f;
 
         UnityEngine.Cursor.lockState = CursorLockMode.Locked;
+        myRB = GetComponent<Rigidbody>();
 
     }
 
@@ -32,7 +41,7 @@ public class playerController3D : MonoBehaviour
         //first we take a vector from the camera transform then use TransformDirection to convert it from
         //local to world space. Then we draw a ray using that vector to display player look (forward) and left/right 
         Vector3 playerLook = myCam.transform.TransformDirection(Vector3.forward);
-        Debug.DrawRay(myCam.transform.position, playerLook*2f, Color.green, .3f);
+        Debug.DrawRay(transform.position, playerLook*2f, Color.green, .3f);
 
         //for the left/ right vectors we do the same but with the corresponding vector.left or vector.right shorthand
         //then we only pull two of the 3 values since we want to render on a flat 2D plane, not full 3D space
@@ -51,12 +60,11 @@ public class playerController3D : MonoBehaviour
 
         //whenever working with vectors for movement or rotation it's good practice
         //to .normalized them - this sets their magnitude equal to 1 and makes them cleaner to work with
-        Vector3 inputDir = transform.rotation * new Vector3(x, 0f, z).normalized;
+        myDir = transform.rotation * new Vector3(x, 0f, z).normalized;
 
         //after normalizing we multiply the vector by our speed variable to set our player speed in a
         //clean and consistent way. We also set the Translate in Space.World, since our input axes give us world coordinates
-        transform.Translate(inputDir*speed, Space.World);
-        Debug.DrawRay(this.transform.position, inputDir * 5f, Color.yellow, 20f);
+        Debug.DrawRay(this.transform.position, myDir * 5f, Color.yellow, 20f);
 
 
         //rotations Y&X are pulled from mouse look, go into camera controls
@@ -77,6 +85,50 @@ public class playerController3D : MonoBehaviour
         transform.rotation = Quaternion.Euler(0f, rotY, 0f);
         myCam.transform.rotation = Quaternion.Euler(-rotX, rotY, 0f);
 
+    }
+
+    void FixedUpdate()
+    {
+        //check for spacebar up to jump
+        if (Input.GetKeyUp(KeyCode.Space) && grounded) { PlayerJump(); }
+
+        //add force from the player direction
+        Vector3 currentDir = Direction();
+        myRB.AddForce(currentDir * speed, ForceMode.VelocityChange);
+        Debug.Log("myDir: " + currentDir);
+
+
 
     }
+
+    //check for collisions with ground and enemies
+    void OnCollisionStay(Collision collision)
+    {
+        if(collision.gameObject.name == "ground") { grounded = true; }
+    }
+
+    //collision exit fires off when we stop colliding so it's
+    //useful to check when the player leaves the ground (AKA jumps)
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.name == "ground") { grounded = false; }
+    }
+
+    //simple add force up on jump
+    void PlayerJump()
+    {
+        myRB.AddForce(new Vector3(0f, jumpForce, 0f), ForceMode.VelocityChange);
+    }
+
+    Vector3 Direction()
+    {
+        //float x & z control the translate/movement inputs, taken from Unity preferences
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+
+        myDir = new Vector3(x, 0f, z);
+
+        return myDir;
+    }
+
 }
